7d2a4dd50423639508f2487a827b07ab1ec50a3e
parxdoxe <dumoulin.florian22@gmail.com>
2022-06-29 11:40:26 +0200

    filtre class active

--- src/Card.js
+++ src/Card.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios'
 
 import './Card.css'
 import { NavLink } from 'react-router-dom';
--- src/Header.js
+++ src/Header.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.css'
 
 const Header = () => {
+
+
+    const [isActive, setIsActive] = useState(1)
+
+     const handleChange = (index) => {
+        setIsActive(index)
+    }
+
     return (
         <header>
             <h1>Découvrez notre <div className='skew'>menu.</div></h1>
             <div className="onglet">
-                <button className='active'>Tout</button>
-                <button>Viandes</button>
-                <button>Legumes</button>
+                <button value="all" onClick={() => handleChange(1)} className={isActive === 1 ? "active" : ""}>Tout</button>
+                <button value="meat" onClick={() => handleChange(2)} className={isActive === 2 ? "active" : ""}>Viandes</button>
+                <button value="flower" onClick={() => handleChange(3)} className={isActive === 3 ? "active" : ""}>Legumes</button>
             </div>
         </header>
     );
